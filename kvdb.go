443da0a59989b43becd2ab3bb068e06d833a6caf
Riadh <22998716+KiKoS0@users.noreply.github.com>
2026-02-25 04:20:35 +0100

    bump pebbledb to v2

--- kvdb.go
+++ kvdb.go
@@ -4,8 +4,8 @@ import (
 	"os"
 	"sync/atomic"
 
-	"github.com/cockroachdb/pebble"
-	"github.com/cockroachdb/pebble/vfs"
+	"github.com/cockroachdb/pebble/v2"
+	"github.com/cockroachdb/pebble/v2/vfs"
 	"github.com/google/uuid"
 )
 
